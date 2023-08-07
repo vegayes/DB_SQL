d1ce6ac2b63878625af136fe18a481b4b66428e6
unknown <vegayes@naver.com>
2023-08-07 17:48:53 +0900

    hello

--- 03_GROUP BY_HAVING.sql
+++ 03_GROUP BY_HAVING.sql
@@ -62,7 +62,7 @@ ORDER BY "인원 수"; -- 별칭 사용 O (SELECT절 해석 완료)
  * 5. SELECT 컬럼명 AS 별칭, 계산식, 함수식
  * 1. FROM 참조할 테이블명
  * 2. WHERE 컬럼명 | 함수식 비교연산자 비교값
- * 3. GROUPT BY 그룹을 묶을 컬럼명
+ * 3. GROUP BY 그룹을 묶을 컬럼명
  * 4. HAVING 그룹함수식 비교연산자 비교값
  * 6. ORDER BY 컬럼명 | 별칭 | 컬럼순번 정렬방식 [NULLS FIRST | LAST];
  * 
